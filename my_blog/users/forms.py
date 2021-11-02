daf1e9ec71d6a2082bbf3838f44c0c0d0ddcc333
Aurav <aurav.tomar@gmail.com>
2021-11-02 13:03:40 +0530

    update user profile after resizing images

--- my_blog/users/forms.py
+++ my_blog/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # we create a new form that inherits from the original form and adds new fields to it
 
@@ -10,3 +11,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- my_blog/users/views.py
+++ my_blog/users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -37,4 +37,28 @@ def register(request):
 # adds functionality that the user can't go to the profile view if they aren't logged in
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance = request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
+
+
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+
+            messages.success(request, f'Your account has been updated.')
+
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+
+    return render(request, 'users/profile.html', context)
