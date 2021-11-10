6d0a9d5e33868c6987a9e54db6729b040c25d699
Aurav <aurav.tomar@gmail.com>
2021-11-10 21:36:43 +0530

    paginate the posts, filter posts by user

--- my_blog/myBlog/urls.py
+++ my_blog/myBlog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='myBlog-home'), 
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # expects app/model_viewtype.html
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # expects app/model_form.html
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
