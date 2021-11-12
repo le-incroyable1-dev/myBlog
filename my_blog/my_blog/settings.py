ca8326efe8328ffc04d50e04a391ad83f0da5dad
Aurav <aurav.tomar@gmail.com>
2021-11-12 23:45:11 +0530

    reset password

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -141,3 +141,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'myBlog-home'
 
 LOGIN_URL = 'login'
+
+# set up email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
